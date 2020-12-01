77856e3a73f74c8f033b1bff5b5a06f703ba4da9
Markus Andersson <markusC17@gmail.com>
2020-12-01 20:27:31 +0100

    send message at 12 on friday and tag eveyone

--- bot.js
+++ bot.js
@@ -2,17 +2,15 @@ require("dotenv").config();
 const cron = require('cron');
 
 const Discord = require("discord.js")
-const client = new Discord.Client({
-})
+const client = new Discord.Client({})
 
 
 client.on("ready", () => {
   console.log("Our bot is ready to go!!!!")
 })
-
 let scheduledMessage = new cron.CronJob('00 00 12 * * 5', () => {
-  client.channels.cache.find(channel => channel.name === 'hallo').send("https://tenor.com/view/mufasa-ghost-cousinskeether-mood-gif-14812360");
-  client.channels.cache.find(channel => channel.name === 'hallo').send("@everyone it's friday");
+  client.channels.cache.find(channel => channel.name === 'dankestmemes').send("https://tenor.com/view/mufasa-ghost-cousinskeether-mood-gif-14812360");
+  client.channels.cache.find(channel => channel.name === 'dankestmemes').send("@everyone it's friday");
 });
 
 scheduledMessage.start()
