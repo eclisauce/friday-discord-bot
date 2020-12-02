afaa2a39d092143231bd268a1f5e232cb9526645
Markus Andersson <markusC17@gmail.com>
2020-12-02 10:50:19 +0100

    moved more stuff into env variables

--- bot.js
+++ bot.js
@@ -4,10 +4,11 @@ const cron = require('cron');
 const Discord = require("discord.js")
 const client = new Discord.Client({})
 
-const mufasaGif = "https://tenor.com/view/mufasa-ghost-cousinskeether-mood-gif-14812360";
-const thursdayVideo = "https://www.youtube.com/watch?v=qcYTzV4HCyk";
-const wedensdayVideo = "https://www.youtube.com/watch?v=du-TY1GUFGk";
+const MUFASA_GIF = process.env.MUFASA_GIF;
+const THURSDAY_VIDEO = process.env.THURSDAY_VIDEO;
+const WEDENSDAY_VIDEO = process.env.WEDENSDAY_VIDEO;
 const DISCORD_CHANNEL = process.env.DISCORD_CHANNEL
+const SKION = process.env.SKION
 
 client.on("ready", () => {
   console.log("Our bot is ready to go!!!!")
@@ -27,17 +28,17 @@ client.on('message', msg => {
 });
 
 let scheduledMessageWednesday = new cron.CronJob('00 00 18 * * 3', () => {
-  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(wedensdayVideo);
+  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(WEDENSDAY_VIDEO);
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send("it is Wednesday");
 });
 
 let scheduledMessageThursday = new cron.CronJob('00 00 18 * * 4', () => {
-  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(thursdayVideo);
-  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(process.env.SKION);
+  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(THURSDAY_VIDEO);
+  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(SKION);
 });
 
 let scheduledMessageFriday = new cron.CronJob('00 00 12 * * 5', () => {
-  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(mufasaGif);
+  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(MUFASA_GIF);
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send("@everyone it's friday");
 });
 
