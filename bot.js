b619ca48f868de3f8c25a28a6e05ddefee16e1a3
Markus Andersson <markusC17@gmail.com>
2021-05-28 22:48:23 +0200

    tweaks to sending message

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const cron = require('cron');
 
 const Discord = require("discord.js")
-const client = new Discord.Client({disableEveryone: false})
+const client = new Discord.Client({disableEveryone: False})
 
 const MUFASA_GIF = process.env.MUFASA_GIF;
 const THURSDAY_VIDEO = process.env.THURSDAY_VIDEO;
@@ -12,6 +12,7 @@ const SKION = process.env.SKION;
 const NOT_FRIDAY= process.env.NOT_FRIDAY;
 const HAPPY_FRIDAY= process.env.HAPPY_FRIDAY;
 const MERRY_CHRISTMAS = process.env.MERRY_CHRISTMAS;
+const UFASA_SONG_LINK = process.env.MUFASA_SONG_LINK;
 
 client.on("ready", () => {
   console.log("Our bot is ready to go!!!!")
@@ -21,9 +22,10 @@ client.on('message', msg => {
   const myDate = new Date();
   const time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
   if(msg.content === "!is it friday?") {
-    if(myDate.getDay() == 5) {
+    if(myDate.getDay() === 5) {
       client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send('Yes its friday')
       client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(HAPPY_FRIDAY)
+      client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(SKION)
     }
     else {
       client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send('No its not')
@@ -33,19 +35,20 @@ client.on('message', msg => {
 });
 
 // seconds, minutes, hours, day of month, month, day of week. Time set is in UTC (+1 for real time)
-let scheduledMessageWednesday = new cron.CronJob('00 00 17 * * 3', () => {
+let scheduledMessageWednesday = new cron.CronJob('00 00 16 * * 3', () => {
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(WEDENSDAY_VIDEO);
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send("it is Wednesday");
 });
 
-let scheduledMessageThursday = new cron.CronJob('00 00 17 * * 4', () => {
+let scheduledMessageThursday = new cron.CronJob('00 00 16 * * 4', () => {
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(THURSDAY_VIDEO);
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(SKION);
 });
 
-let scheduledMessageFriday = new cron.CronJob('00 00 11 * * 5', () => {
+let scheduledMessageFriday = new cron.CronJob('00 00 10 * * 5', () => {
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(MUFASA_GIF);
   client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send("@everyone it's friday");
+  client.channels.cache.find(channel => channel.name === DISCORD_CHANNEL).send(MUFASA_SONG_LINK);
 });
 
 let scheduledMessageChristmas = new cron.CronJob('00 00 14 24 11 *', () => {
